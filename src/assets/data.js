2f579cea518fb2878a5d18c21a45957f8590957b
unknown <andreaagudelocardona789@gmail.com.com>
2024-08-22 15:20:18 -0500

    initial local commit

---
+++ src/assets/data.js
@@ -0,0 +1,129 @@
+export const socials = [
+    { 
+       logo: 'bx bxl-linkedin-square',
+       link: 'https://www.linkedin.com/in/andrea-agudelo-cardona-2a6470173/' 
+    }, 
+    { 
+       logo: 'bx bxl-github',
+       link:'https://github.com/Andrea1789'
+    }
+ ];
+ 
+ export const info = [{ text: 'About' }, { text: 'Projects' }];
+ 
+ export const technologies = [
+    {
+       text: 'HTML',
+       logo: 'bx bxl-html5',
+    },  
+    {
+       text: 'CSS',
+       logo: 'bx bxl-css3',
+    },
+    {
+       text: 'JavaScript',
+       logo: 'bx bxl-javascript',
+    },
+    {
+       text: 'Wordpress',
+       logo: 'bx bxl-wordpress',
+    },
+    {
+       text: 'React',
+       logo: 'bx bxl-react',
+    },
+  
+    {
+       text: 'TypeScript',
+       logo: 'bx bxl-typescript',
+    },
+    {
+       text: 'SQL',
+       logo: 'bx bxs-data',
+    },
+    {
+       text: 'Java',
+       logo: 'bx bxl-java',
+    },
+    {
+       text: 'Spring-boot',
+       logo: 'bx bxl-spring-boot',
+    },
+    {
+       text: 'GitHub',
+       logo: 'bx bxl-github',
+    },
+  
+ ];
+ 
+ export const projects = [
+    {
+       title: 'RX Store',
+       description:
+         'Dynamic WordPress website for a fitness supplements brand, designed to showcase their product range and engage health-conscious customers. The site features a clean, modern design with intuitive navigation, allowing users to easily explore different supplement categories and access detailed product information. I integrated e-commerce functionality to enable seamless online purchases and implemented SEO best practices to drive organic traffic.',
+       image: 'RX',
+       color: '#f3c201',
+       link: 'https://rxcolombia.com/'
+    },
+    {
+       title: 'Autopartes',
+       description:
+         "WordPress website for an auto parts store, focused on creating a seamless shopping experience for customers. The project involved designing a visually appealing and user-friendly interface that showcased the store’s wide range of products. I integrated advanced search and filtering options to help customers easily find specific parts, and implemented secure payment gateways to ensure a smooth checkout process. Additionally, I optimized the site for SEO to increase visibility and drive organic traffic, resulting in higher sales and customer satisfaction.",
+       image: 'francia',
+       color: '#1629ab',
+       link: 'https://piezasyautopartesfrancia.com/'
+    },
+    {
+       title: 'Tecnotienda',
+       description:
+         "WordPress project for a tech store, where I developed a modern and responsive e-commerce website tailored to showcase the store's wide range of tech products. The site features a clean, user-friendly design with intuitive navigation, making it easy for customers to browse and purchase products. I customized the theme to align with the store’s branding, integrated WooCommerce for seamless online transactions, and optimized the site for speed and SEO to enhance visibility in search engines. Additionally, I implemented secure payment gateways and product filtering options to improve the overall shopping experience.",
+       image: 'tecnotienda2',
+       color: '#045fb7',
+       link: 'https://tecnotienda.com.co/'
+    },
+   //  {
+   //     title: 'Facebook Clone',
+   //     description:
+   //        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+   //     image: 'image-facebook-clone',
+   //     color: '#146866',
+   //  },
+   //  {
+   //     title: 'Instagram Clone',
+   //     description:
+   //        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+   //     image: 'image-instagram-clone',
+   //     color: '#17193f',
+   //  },
+   //  {
+   //     title: 'Slider',
+   //     description:
+   //        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+   //     image: 'image-slider',
+   //     color: '#bbbbbb',
+   //  },
+   //  {
+   //     title: 'Spotify Clone',
+   //     description:
+   //        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+   //     image: 'image-spotify-clone',
+   //     color: '#49d53c',
+   //  },
+   //  {
+   //     title: 'Youtube Clone',
+   //     description:
+   //        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+   //     image: 'image-youtube-clone',
+   //     color: '#f7030c',
+   //  },
+ ];
+ 
+ export const buttons = [
+    {
+       github: 'bx bxl-github',
+       demo: 'bx bx-desktop',
+       back: 'bx bxs-left-arrow-circle',
+       hrefgithub: 'https://github.com/MilaDev-JS'
+    },
+ ];
+ 
